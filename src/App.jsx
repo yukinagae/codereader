655718620c8fd468923f7a3b4a3df4fae890133b
Yuki Nagae <yuki.nagae1130@gmail.com>
2018-09-03 23:16:59 +0900

    nav + code section

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+import 'bulma/css/bulma.css'
+
+
+class App extends Component {
+    render() {
+    return (
+        <div className="App">
+            <div className="columns">
+                <aside className="column is-2 aside hero is-fullheight">
+                    hoge
+                </aside>
+                <div className="column is-10 code hero is-fullheight">
+                    hoge
+                </div>
+            </div>
+
+        </div>
+    );
+  }
+}
+
+export default App;
